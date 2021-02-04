249207387ff38305b8aca83bce3a500644d734f0
Grigorii Kolychev <kolychevgv@mail.ru>
2021-02-04 22:36:55 +0300

    first commit

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -20,7 +20,22 @@ public class Solution {
         words.add("В");
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        try(BufferedReader fileReader = new BufferedReader(new FileReader(fileName));){
+            while (fileReader.ready()){
+                String string = fileReader.readLine();
+                int counter = 0;
+                String [] splitedString = string.split(" ");
+                for (int i = 0; i < splitedString.length; i++) {
+                    if (words.contains(splitedString[i]))
+                        counter++;
+                }
+                if (counter==2)
+                    System.out.println(string);
+            }
+        }
     }
 }
